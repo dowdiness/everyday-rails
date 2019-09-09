e7eaf6a763d339a503f8f93387d1d814e992da89
koji <koji.ishimoto@gmail.com>
2019-09-09 14:06:36 +0900

    Commit from vscode

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -25,21 +25,7 @@ RSpec.describe Project, type: :model do
     end
   end
 
-  it "does not allow duplicate project names per user" do
-    user = FactoryBot.create(:user)
-
-    user.projects.create(
-      name: "Test Project",
-    )
-
-    new_project = user.projects.build(
-      name: "Test Project",
-    )
-
-    new_project.valid?
-
-    expect(new_project.errors[:name]).to include("has already been taken")
-  end
+  it { is_expected.to validate_uniqueness_of(:name).scoped_to(:user_id) }
 
   it "allows two users to share a project name" do
     user = FactoryBot.create(:user)
